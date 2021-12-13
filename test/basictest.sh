9df2b730abd64a382d51c0ac25b783b5692c70b5
Adrian Sutherland <adrian@sutherlandonline.org>
2021-12-13 17:00:03 +0000

    Test Fixes

--- test/basictest.sh
+++ test/basictest.sh
@@ -4,7 +4,7 @@ set -e
 ./herccontrol "" -w "USER DSC LOGOFF AS AUTOLOG1"
 ./herccontrol "/enable all" -w "COMMAND COMPLETE"
 ./herccontrol "/cp disc" -w "^VM/370 Online"
-./herccontrol "/logon cmsuser cmsuser" -w "^CMS VERSION"
+./herccontrol "/logon cmsuser cmsuser" -w "^CMS"
 ./herccontrol "/" -w "^Ready"
 ./herccontrol "/listf * * a" -w "^Ready"
 ./herccontrol "/logoff" -w "^VM/370 Online"
